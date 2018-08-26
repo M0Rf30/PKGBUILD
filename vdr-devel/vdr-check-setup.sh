9e3fdcb89131d8fe610d9b8e6fa165700e530dc4
Gianluca Boiano <morf3089@gmail.com>
2018-08-26 23:18:42 +0200

    simonpi: updated checksum

--- vdr-devel/vdr-check-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cfg=/var/lib/vdr/channels.conf
-if [ ! -s $cfg ] ; then
-    echo "$cfg is not valid, use \"w_scan -t 3 -o 21\" from the w_scan package to create one." >&2
-    exit 2
-else
-    chown vdr:video $cfg && chmod 644 $cfg
-fi
--- vdr-devel/vdr-shutdown.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# To enable this script, pass it to vdr with the -s option, eg.
-# "-s vdr-shutdown.sh" (sans quotes) in VDR_OPTIONS in /etc/sysconfig/vdr.
-
-echo ${1:-0} > /var/run/vdr/next-timer
-exec sudo /usr/bin/shutdown -P now
